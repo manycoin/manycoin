74a3a7b228fea5445488d80052aa6998d753561f
huzhengkui <huzhengkui@126.com>
2018-06-12 10:21:41 +0800

    update config

---
+++ client/attach.sh
@@ -0,0 +1,6 @@
+okcPath="./gokc.ipc"
+
+if [ -e $okcPath ]; then
+./okc at ./gokc.ipc
+fi
+
---
+++ client/console.sh
@@ -0,0 +1,11 @@
+okcPath="./keystore"
+
+if [ -e $okcPath ]; then
+echo "already init"
+else
+echo "init"
+./okc --datadir ./ rj config.json
+fi
+
+echo "run okc"
+./okc --datadir ./ cs 2>>./okc.log
